53f6dce1d74c16415b526f73ce3b49410496dfae
Zuka <atharvmotghare07@gmail.com>
2025-10-19 12:23:46 +0530

    feat: add terminal integration

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,31 @@
+-- ============================================================================
+-- LUA/PLUGINS/TOGGLETERM.LUA - Terminal Integration
+-- ============================================================================
+
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+    open_mapping = [[<C-\>]],
+    hide_numbers = true,
+    shade_terminals = true,
+    start_in_insert = true,
+    insert_mappings = true,
+    terminal_mappings = true,
+    persist_size = true,
+    persist_mode = true,
+    direction = "float",
+    close_on_exit = true,
+    shell = vim.o.shell,
+    float_opts = {
+      border = "curved",
+      winblend = 0,
+    },
+  },
+}
