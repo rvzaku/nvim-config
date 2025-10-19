d5d3a57655fd4148bf5812f5a5120b44f5f3864a
Zuka <atharvmotghare07@gmail.com>
2025-10-19 12:30:50 +0530

    chore: update dap for debugging

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,4 +1,3 @@
-
 -- ============================================================================
 -- LUA/PLUGINS/DAP.LUA - Debugging Support
 -- ============================================================================
@@ -11,49 +10,113 @@ return {
     "nvim-neotest/nvim-nio",
   },
   config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-    
-    dapui.setup()
-    require("nvim-dap-virtual-text").setup()
-    
-    -- Auto-open DAP UI
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
-    
-    -- C/C++/Rust adapter
+    local dap = require "dap"
+    local dapui = require "dapui"
+
+    -- DAP UI setup
+    dapui.setup {
+      icons = { expanded = "", collapsed = "", current_frame = "" },
+      mappings = {
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
+      },
+      layouts = {
+        {
+          elements = {
+            { id = "scopes", size = 0.25 },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks", size = 0.25 },
+            { id = "watches", size = 0.25 },
+          },
+          size = 40,
+          position = "left",
+        },
+        {
+          elements = {
+            { id = "repl", size = 0.5 },
+            { id = "console", size = 0.5 },
+          },
+          size = 10,
+          position = "bottom",
+        },
+      },
+      floating = {
+        max_height = nil,
+        max_width = nil,
+        border = "rounded",
+        mappings = {
+          close = { "q", "<Esc>" },
+        },
+      },
+    }
+
+    -- Virtual text setup
+    require("nvim-dap-virtual-text").setup {
+      enabled = true,
+      enabled_commands = true,
+      highlight_changed_variables = true,
+      highlight_new_as_changed = false,
+      show_stop_reason = true,
+      commented = false,
+      only_first_definition = true,
+      all_references = false,
+      filter_references_pattern = "<module",
+      virt_text_pos = "eol",
+      all_frames = false,
+      virt_lines = false,
+      virt_text_win_col = nil,
+    }
+
+    -- Auto-open/close DAP UI
+    dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+    dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+    dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+
+    -- DAP signs
+    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticError", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DiagnosticWarn", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapStopped", { text = "󰁕", texthl = "DiagnosticOk", linehl = "DebugHighlight", numhl = "" })
+    vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DiagnosticError", linehl = "", numhl = "" })
+
+    -- === ADAPTER CONFIGURATIONS ===
+
+    -- C/C++/Rust (codelldb)
     dap.adapters.codelldb = {
       type = "server",
       port = "${port}",
       executable = {
-        command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+        command = vim.fn.stdpath "data" .. "/mason/bin/codelldb",
         args = { "--port", "${port}" },
       },
     }
-    
+
     dap.configurations.cpp = {
       {
-        name = "Launch",
+        name = "Launch file",
         type = "codelldb",
         request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
+        program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
         cwd = "${workspaceFolder}",
         stopOnEntry = false,
+        args = {},
+      },
+      {
+        name = "Attach to process",
+        type = "codelldb",
+        request = "attach",
+        pid = require("dap.utils").pick_process,
+        args = {},
       },
     }
     dap.configurations.c = dap.configurations.cpp
     dap.configurations.rust = dap.configurations.cpp
-    
-    -- Go adapter
+
+    -- Go (delve)
     dap.adapters.delve = {
       type = "server",
       port = "${port}",
@@ -62,7 +125,7 @@ return {
         args = { "dap", "-l", "127.0.0.1:${port}" },
       },
     }
-    
+
     dap.configurations.go = {
       {
         type = "delve",
@@ -70,23 +133,73 @@ return {
         request = "launch",
         program = "${file}",
       },
+      {
+        type = "delve",
+        name = "Debug test",
+        request = "launch",
+        mode = "test",
+        program = "${file}",
+      },
+      {
+        type = "delve",
+        name = "Debug test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+      },
     }
-    
-    -- Python adapter
+
+    -- Python (debugpy)
     dap.adapters.python = {
       type = "executable",
       command = "python3",
       args = { "-m", "debugpy.adapter" },
     }
-    
+
     dap.configurations.python = {
       {
         type = "python",
         request = "launch",
         name = "Launch file",
         program = "${file}",
-        pythonPath = "python3",
+        pythonPath = function()
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+            return cwd .. "/venv/bin/python"
+          elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+            return cwd .. "/.venv/bin/python"
+          else
+            return "python3"
+          end
+        end,
       },
     }
+
+    -- Node.js (node2)
+    dap.adapters.node2 = {
+      type = "executable",
+      command = "node",
+      args = { vim.fn.stdpath "data" .. "/mason/packages/node-debug2-adapter/out/src/nodeDebug.js" },
+    }
+
+    dap.configurations.javascript = {
+      {
+        name = "Launch",
+        type = "node2",
+        request = "launch",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        console = "integratedTerminal",
+      },
+      {
+        name = "Attach to process",
+        type = "node2",
+        request = "attach",
+        processId = require("dap.utils").pick_process,
+      },
+    }
+    dap.configurations.typescript = dap.configurations.javascript
   end,
 }
