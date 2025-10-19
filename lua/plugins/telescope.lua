6dd24abaf0ce3deef15535a77fbec4664152af5c
Zuka <atharvmotghare07@gmail.com>
2025-10-19 12:18:17 +0530

    chore: update telescope configuration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,44 @@
 
 return {
   "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
+  dependencies = {
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  opts = function(_, opts)
+    local actions = require "telescope.actions"
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
       prompt_prefix = "  ",
       selection_caret = " ",
       path_display = { "smart" },
-      file_ignore_patterns = { "node_modules", ".git", "target" },
-    },
-  },
+      file_ignore_patterns = {
+        "node_modules",
+        ".git/",
+        "target/",
+        "build/",
+        "%.jpg",
+        "%.jpeg",
+        "%.png",
+        "%.svg",
+        "%.otf",
+        "%.ttf",
+      },
+      mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+        },
+        n = {
+          ["q"] = actions.close,
+        },
+      },
+    })
+
+    return opts
+  end,
+  config = function(plugin, opts)
+    require("telescope").setup(opts)
+    require("telescope").load_extension "fzf"
+  end,
 }
