7ad5f3af8de615a13cd7e7dd94e86a0639a3a3e5
Zuka <atharvmotghare07@gmail.com>
2025-10-19 12:15:43 +0530

    chore: update none-ls formatting configuration

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,13 @@
 -- ============================================================================
--- LUA/PLUGINS/NONE-LS.LUA - Linting & Formatting (FIXED)
+-- LUA/PLUGINS/NONE-LS.LUA - Formatting
 -- ============================================================================
 
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
-    local null_ls = require("null-ls")
-    
-    -- Initialize sources table if it doesn't exist
+    local null_ls = require "null-ls"
     opts.sources = opts.sources or {}
-    
-    -- Formatting sources (these are stable)
+
     table.insert(opts.sources, null_ls.builtins.formatting.stylua)
     table.insert(opts.sources, null_ls.builtins.formatting.prettier)
     table.insert(opts.sources, null_ls.builtins.formatting.black)
@@ -18,10 +15,7 @@ return {
     table.insert(opts.sources, null_ls.builtins.formatting.clang_format)
     table.insert(opts.sources, null_ls.builtins.formatting.gofumpt)
     table.insert(opts.sources, null_ls.builtins.formatting.shfmt)
-    
-    -- NOTE: Diagnostics removed - use LSP servers instead (bashls, eslint, etc.)
-    -- If you need shellcheck, install it via mason and use bashls
-    
+
     return opts
   end,
 }
