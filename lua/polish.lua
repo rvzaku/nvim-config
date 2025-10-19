68069fe632016dc729aa83470983c1c67009d411
Zuka <atharvmotghare07@gmail.com>
2025-10-19 12:32:07 +0530

    chore: updated polish

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,26 +1,66 @@
 -- ============================================================================
--- LUA/POLISH.LUA - Final Customizations
+-- LUA/POLISH.LUA - Final Customizations & Autocommands
 -- ============================================================================
 
 -- Set colorscheme
-vim.cmd.colorscheme("astrodark")
+vim.cmd.colorscheme "astrodark"
 
--- Additional autocommands
+-- Remove trailing whitespace on save
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   callback = function()
-    -- Remove trailing whitespace on save
-    local save_cursor = vim.fn.getpos(".")
-    vim.cmd([[%s/\s\+$//e]])
+    local save_cursor = vim.fn.getpos "."
+    vim.cmd [[%s/\s\+$//e]]
     vim.fn.setpos(".", save_cursor)
   end,
 })
 
--- FileType-specific settings
+-- FileType-specific indentation
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp", "java" },
+  pattern = { "c", "cpp", "java", "go" },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
+  end,
+})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function() vim.highlight.on_yank { higroup = "Visual", timeout = 200 } end,
+})
+
+-- Auto-create directories when saving files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(event)
+    if event.match:match "^%w%w+://" then return end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-- Remember cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+  end,
+})
+
+-- Close certain filetypes with 'q'
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "lspinfo",
+    "checkhealth",
+    "startuptime",
+    "fugitive",
+    "git",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
   end,
 })
