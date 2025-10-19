a073283a3aa78dbd3b9190a3aec0ea92feb356bd
Zuka <atharvmotghare07@gmail.com>
2025-10-19 12:19:34 +0530

    chore: update neo-tree configuration

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,3 @@
-
 -- ============================================================================
 -- LUA/PLUGINS/NEO-TREE.LUA - File Explorer
 -- ============================================================================
@@ -6,12 +5,28 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    close_if_last_window = true,
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
         hide_gitignored = false,
+        hide_by_name = {
+          ".git",
+          "node_modules",
+        },
       },
-      follow_current_file = { enabled = true },
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      position = "left",
+      width = 30,
     },
   },
 }
