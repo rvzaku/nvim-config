2802fa63e685206ecd4922a992c89c641524c365
Zuka <atharvmotghare07@gmail.com>
2025-10-19 12:29:45 +0530

    chore + feat: added gitsigns and updated code-runner & lazygit

--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -4,7 +4,7 @@
 
 return {
   "CRAG666/code_runner.nvim",
-  cmd = { "RunCode", "RunFile", "RunClose" },
+  cmd = { "RunCode", "RunFile", "RunClose", "RunProject" },
   opts = {
     mode = "term",
     focus = false,
@@ -14,10 +14,26 @@ return {
       size = 12,
     },
     filetype = {
-      c = "cd $dir && gcc $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
-      cpp = "cd $dir && g++ -std=c++17 $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
-      java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-      rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
+      c = {
+        "cd $dir &&",
+        "gcc $fileName -o $fileNameWithoutExt &&",
+        "$dir/$fileNameWithoutExt",
+      },
+      cpp = {
+        "cd $dir &&",
+        "g++ -std=c++17 $fileName -o $fileNameWithoutExt &&",
+        "$dir/$fileNameWithoutExt",
+      },
+      java = {
+        "cd $dir &&",
+        "javac $fileName &&",
+        "java $fileNameWithoutExt",
+      },
+      rust = {
+        "cd $dir &&",
+        "rustc $fileName &&",
+        "$dir/$fileNameWithoutExt",
+      },
       go = "go run $fileName",
       python = "python3 -u $fileName",
       javascript = "node $fileName",
@@ -25,6 +41,23 @@ return {
       sh = "bash $fileName",
       bash = "bash $fileName",
       lua = "lua $fileName",
+      r = "Rscript $fileName",
+      ruby = "ruby $fileName",
+      perl = "perl $fileName",
+    },
+    project = {
+      ["Cargo.toml"] = "cargo run",
+      ["package.json"] = "npm start",
+      ["go.mod"] = "go run .",
+      ["Makefile"] = "make run",
+      ["CMakeLists.txt"] = {
+        "cd $dir &&",
+        "mkdir -p build &&",
+        "cd build &&",
+        "cmake .. &&",
+        "make &&",
+        "./main",
+      },
     },
   },
 }
